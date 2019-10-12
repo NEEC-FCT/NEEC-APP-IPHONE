0309af38803e0457021c1460b733fa66d3b7e575
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-10-12 20:42:05 +0100

    App feita

--- Example/Controllers/ViewController.m
+++ Example/Controllers/ViewController.m
@@ -14,6 +14,8 @@
 #import "ClockViewController.h"
 #import "MapViewController.h"
 #import "GlobeViewController.h"
+@import SystemConfiguration.CaptiveNetwork;
+#include <SystemConfiguration/SystemConfiguration.h>
 
 
 @implementation ViewController
@@ -22,14 +24,15 @@
 #pragma mark - UIViewController
 
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     // Instance creation.
     ESTabBarController *tabBarController = [[ESTabBarController alloc] initWithTabIconNames:@[@"archive",
-                                                                                              @"clock",
-                                                                                              @"target",
                                                                                               @"map",
+                                                                                              @"target",
+                                                                                              @"clock",
                                                                                               @"globe"]];
     
     // Add child view controller.
@@ -46,18 +49,17 @@
     [tabBarController setViewController:[[ArchiveViewController alloc] init]
                                 atIndex:0];
     
-    [tabBarController setViewController:[[ClockViewController alloc] init]
-                                atIndex:1];
+   
     
     [tabBarController setViewController:[[MapViewController alloc] init]
-                                atIndex:3];
+                                atIndex:1];
     
     [tabBarController setViewController:[[GlobeViewController alloc] init]
                                 atIndex:4];
     
     // Colors.
     
-    tabBarController.selectedColor = [UIColor colorWithHexString:@"#CD5B45"];
+    tabBarController.selectedColor = [UIColor colorWithHexString:@"#8BC12B"];
     tabBarController.buttonsBackgroundColor = [UIColor colorWithHexString:@"#F6EBE0"];
     
     // Highlighted buttons.
@@ -69,14 +71,106 @@
     __weak typeof (self) weakSelf = self;
     
     [tabBarController setAction:^{
-        [weakSelf showAlertViewWithTitle:@"Highlighted"
-                                 message:@"You have chosen the highlighted tab button."];
+        NSString * cargo = [[NSUserDefaults standardUserDefaults] objectForKey: @"cargo"];
+        
+        NSString *wifiName = nil;
+        NSArray *ifs = (__bridge_transfer id)CNCopySupportedInterfaces();
+        for (NSString *ifnam in ifs) {
+            NSDictionary *info = (__bridge_transfer id)CNCopyCurrentNetworkInfo((__bridge CFStringRef)ifnam);
+            if (info[@"SSID"]) {
+                wifiName = info[@"SSID"];
+            }
+        }
+        
+
+        if ([cargo containsString:@"Junior"] || [cargo containsString:@"junior"]) {
+            [weakSelf showAlertViewWithTitle:@"Erro"
+                                     message:@"Os Juniores não podem abrir a porta"];
+        }
+        
+        
+        else  if ([cargo containsString:@"Senior"] || [cargo containsString:@"senior"]) {
+          
+            if ([wifiName containsString:@"NEEC"]) {
+               
+                
+                NSString * token = [[NSUserDefaults standardUserDefaults] objectForKey: @"token"];
+                NSString * email = [[NSUserDefaults standardUserDefaults] objectForKey: @"email"];
+                NSString * uuid = [[NSUserDefaults standardUserDefaults] objectForKey: @"IMEI"];;
+                //printf("%s", [uuid UTF8String]);
+                
+                
+                NSMutableURLRequest *request =[NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://neecapp.neec-fct.com/portav2.php?"]];
+                [request setHTTPMethod:@"POST"];
+                
+                NSString *post =[[NSString alloc] initWithFormat:@"token=%@&cargo=%@&IMEI=%@&email=%@",token,cargo,uuid,email];
+                printf("%s", [post UTF8String]);
+                [request setHTTPBody:[post dataUsingEncoding:NSUTF8StringEncoding]];
+                
+                NSURLResponse *response;
+                NSError *err;
+                NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&err];
+                NSString *myString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+                
+                printf("%s", [myString UTF8String]);
+                if ([myString rangeOfString:@"true"].location == NSNotFound) {
+                    [weakSelf showAlertViewWithTitle:@"Erro"
+                                             message:@"Ocorreu um erro por favor faça logout"];
+                } else {
+                    [weakSelf showAlertViewWithTitle:@"Sucesso"
+                                             message:@"A porta vai abrir dentro de momentos"];
+                }
+                
+            } else {
+                printf("%s", [wifiName UTF8String]);
+                [weakSelf showAlertViewWithTitle:@"Erro"
+                                         message:@"Os seniores tem de estar ligados ao router do NEEC"];
+            }
+            
+        }
+        else {
+  
+            NSString * token = [[NSUserDefaults standardUserDefaults] objectForKey: @"token"];
+            NSString * email = [[NSUserDefaults standardUserDefaults] objectForKey: @"email"];
+            NSString * uuid = [[NSUserDefaults standardUserDefaults] objectForKey: @"IMEI"];;
+            //printf("%s", [uuid UTF8String]);
+            
+            
+            NSMutableURLRequest *request =[NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://neecapp.neec-fct.com/portav2.php?"]];
+            [request setHTTPMethod:@"POST"];
+            
+            NSString *post =[[NSString alloc] initWithFormat:@"token=%@&cargo=%@&IMEI=%@&email=%@",token,cargo,uuid,email];
+            printf("%s", [post UTF8String]);
+            [request setHTTPBody:[post dataUsingEncoding:NSUTF8StringEncoding]];
+            
+            NSURLResponse *response;
+            NSError *err;
+            NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&err];
+            NSString *myString = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+            
+            printf("%s", [myString UTF8String]);
+            if ([myString rangeOfString:@"true"].location == NSNotFound) {
+                [weakSelf showAlertViewWithTitle:@"Erro"
+                                         message:@"Ocorreu um erro por favor faça logout"];
+            } else {
+                [weakSelf showAlertViewWithTitle:@"Sucesso"
+                                         message:@"A porta vai abrir dentro de momentos"];
+            }
+        }
+        
+
+        
+     
+        
+        
+    
     } atIndex:2];
     
     [tabBarController setAction:^{
-        [weakSelf showAlertViewWithTitle:@"Globe"
-                                 message:@"You have chosen the globe view controller."];
-    } atIndex:4];
+        [weakSelf showAlertViewWithTitle:@"Brevemente"
+                                 message:@"Ainda a ser desenvolvida"];
+    } atIndex:3];
+    
 }
 
 
